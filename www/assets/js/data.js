bd7060f8ede95fdf3c6a7142bbfbe5f4b07c76e2
Alberto Permuy Leal <alberto.permuy@gmail.com>
2022-11-26 12:17:45 +0100

    Primeira aportación

--- www/assets/js/data.js
+++ www/assets/js/data.js
@@ -6,15 +6,15 @@ var elementos = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    -8.25580,
-                    43.46196
+                    -8.24129,
+                    43.45521
                 ]
             },
             "type": "Feature",
             "properties": {
-                "title": "O banco dos namorados",
-                "person": "Luisa: <b>a da Pitusa</b>",
-                "neighborhood": "sanxulian",
+                "title": "Campo de fúbol vello",
+                "person": "Evaristo",
+                "neighborhood": "mea",
                 "paragraph_1" : "",
                 "paragraph_2" : "",
                 "sound_file": "",
@@ -27,18 +27,18 @@ var elementos = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    -8.23345,
-                    43.45460
+                    -8.258,
+                    43.461
                 ]
             },
             "type": "Feature",
             "properties": {
-                "title": "Acrivo: a árbore da vida",
+                "title": "Matadoiro",
                 "person": "Pepe Bao : <b>O da collona</b>",
-                "neighborhood": "oseixo",
+                "neighborhood": "sanxulian",
                 "paragraph_1" : "",
-                "paragraph_2" : "reto do peirao, Xoan o da Conexa constríu un banco para a súa namorada...",
-                "sound_file": "https://www.arredordorural.org/sites/default/files/IIIENCONTRO_SABERES_15_6_2022.mp3",
+                "paragraph_2" : "",
+                "sound_file": ".mp3",
                 "image_file": ""                 
             }            
         }
